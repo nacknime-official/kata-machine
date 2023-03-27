71523d2d0213f2c14ae57c3c369f1847c06a42e9
nacknime-official <nacknime.official@gmail.com>
2023-03-27 10:55:58 +0300

    add day 7

---
+++ src/day7/ArrayList.ts
@@ -0,0 +1,99 @@
+export default class ArrayList<T> {
+    public length: number;
+    private capacity: number
+    private arr: Array<T | undefined>;
+
+    
+
+    constructor(capacity: number) {
+        this.length = 0;
+        this.capacity = capacity;
+        this.arr = new Array<T | undefined>(capacity);
+    }
+
+    prepend(item: T): void {
+        return this.insertAt(item, 0);
+    }
+
+    // [1, 2, 3, 4, _, _]
+    //        ^
+    //        33
+    //
+    //            
+    // [1, 2, 33, 3, 4, _]
+    private shiftRight(from: number): void {
+        for (let i = this.length; i > from; i--) {
+            this.arr[i] = this.arr[i - 1];
+        }
+    }
+
+    private increaseArrSize(): void {
+        this.capacity *= 2;
+
+        const newArr = new Array<T | undefined>(this.capacity);
+        for (let i = 0; i < this.length; i++) {
+            newArr[i] = this.arr[i];
+        }
+
+        this.arr = newArr;
+    }
+
+    insertAt(item: T, idx: number): void {
+        if (idx > this.length || idx < 0) {
+            return;
+        }
+
+        if (this.length === this.capacity) {
+            this.increaseArrSize();
+        }
+
+        this.shiftRight(idx)
+        this.arr[idx] = item;
+
+        this.length++;
+    }
+
+    append(item: T): void {
+        return this.insertAt(item, this.length);
+    }
+
+    remove(item: T): T | undefined {
+        for (let i = 0; i < this.length; i++) {
+            if (item === this.arr[i]) {
+                return this.removeAt(i);
+            }
+        }
+        return;
+    }
+
+    get(idx: number): T | undefined {
+        if (idx >= this.length || idx < 0) {
+            return;
+        }
+        return this.arr[idx];
+    }
+
+
+    // [1, 2, 3, 4, _, _]
+    //     ^
+    //   remove
+    //
+    // [1, 3, 4, _, _, _]
+    private shiftLeft(from: number): void {
+        for (let i = from; i < this.length; i++) {
+            this.arr[i] = this.arr[i + 1];
+        }
+    }
+
+    removeAt(idx: number): T | undefined {
+        if (idx >= this.length || idx < 0) {
+            return;
+        }
+
+        const removingEl = this.arr[idx];
+        this.shiftLeft(idx);
+        
+        this.length--;
+        return removingEl;
+    }
+}
---
+++ src/day7/Stack.ts
@@ -0,0 +1,46 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+}
+
+// a <- b <- c <- d
+//                ^
+//                h
+
+export default class Stack<T> {
+    public length: number;
+    private head?: Node<T>;
+
+    
+
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+    }
+
+    push(item: T): void {
+        const node = {value: item} as Node<T>;
+
+        node.next = this.head;
+        this.head = node;
+
+        this.length++;
+    }
+
+    pop(): T | undefined {
+        if (!this.head) {
+            return;
+        }
+
+        const poppingNode = this.head;
+        this.head = this.head.next;
+        
+        poppingNode.next = undefined;
+        this.length--;
+        return poppingNode.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
