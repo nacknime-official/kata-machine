4397711c26ad7178b28bfe1fa4f348e908193c57
nacknime-official <nacknime.official@gmail.com>
2023-04-03 00:10:35 +0300

    add day 13

---
+++ src/day13/Queue.ts
@@ -0,0 +1,48 @@
+type Node<T> = {
+	value: T;
+	next?: Node<T>;
+}
+
+export default class Queue<T> {
+	public length: number;
+	private head?: Node<T>;
+	private tail?: Node<T>;
+
+
+
+	constructor() {
+		this.length = 0;
+		this.head = undefined;
+		this.tail = undefined;
+	}
+
+	enqueue(item: T): void {
+		this.length++
+		const node = { value: item } as Node<T>;
+
+		if (!this.head || !this.tail) {
+			this.head = this.tail = node;
+			return;
+		}
+
+		this.tail.next = node;
+		this.tail = node;
+	}
+
+	deque(): T | undefined {
+		if (!this.head) {
+			return;
+		}
+		this.length--;
+		const dequedNode = this.head;
+
+		this.head = this.head.next;
+		dequedNode.next = undefined;
+
+		return dequedNode.value;
+	}
+
+	peek(): T | undefined {
+		return this.head?.value;
+	}
+}
---
+++ src/day13/QuickSort.ts
@@ -0,0 +1,37 @@
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
+
+    const pivotIdx = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotIdx);
+    qs(arr, pivotIdx + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivotIdx = Math.floor((lo + hi) / 2);
+    const pivot = arr[pivotIdx];
+    let i = lo;
+    let j = hi;
+
+    while (true) {
+        while (arr[i] < pivot) {
+            i++;
+        }
+        while (arr[j] > pivot) {
+            j--;
+        }
+
+        if (i >= j) {
+            return j
+        }
+        const tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+}
