87759d0860aca35ccaf4126f00399d098bffa20f
nacknime-official <nacknime.official@gmail.com>
2023-04-03 23:16:49 +0300

    add day 14

---
+++ src/day14/ArrayList.ts
@@ -0,0 +1,92 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+}
+
+export default class ArrayList<T> {
+  public length: number;
+  private capacity: number;
+  private arr: Array<T | undefined>;
+
+
+  constructor(capacity: number) {
+    this.length = 0;
+    this.capacity = capacity;
+    this.arr = new Array<T | undefined>(capacity);
+  }
+
+  prepend(item: T): void {
+    return this.insertAt(item, 0);
+  }
+
+  insertAt(item: T, idx: number): void {
+    if (idx > this.length || idx < 0) {
+      return;
+    }
+
+    if (this.length === this.capacity) {
+      this.increaceArrSize();
+    }
+
+    this.shiftRight(idx);
+    this.arr[idx] = item;
+
+    this.length++;
+  }
+
+  append(item: T): void {
+    return this.insertAt(item, this.length);
+  }
+
+  remove(item: T): T | undefined {
+    for (let i = 0; i < this.length; i++) {
+      if (item === this.arr[i]) {
+        return this.removeAt(i);
+      }
+    }
+    return;
+  }
+
+  get(idx: number): T | undefined {
+    if (idx >= this.length || idx < 0) {
+      return;
+    }
+    return this.arr[idx];
+  }
+
+  removeAt(idx: number): T | undefined {
+    if (idx >= this.length || idx < 0) {
+      return;
+    }
+    const removingEl = this.arr[idx];
+
+    this.shiftLeft(idx);
+    this.arr[this.length - 1] = undefined;
+
+    this.length--;
+    return removingEl;
+  }
+
+  private increaceArrSize(): void {
+    this.capacity *= 2;
+    const newArr = new Array<T | undefined>(this.capacity);
+
+    for (let i = 0; i < this.length; i++) {
+      newArr[i] = this.arr[i];
+    }
+
+    this.arr = newArr;
+  }
+
+  private shiftLeft(from: number): void {
+    for (let i = from; i < this.length - 1; i++) {
+      this.arr[i] = this.arr[i + 1];
+    }
+  }
+
+  private shiftRight(from: number): void {
+    for (let i = this.length; i > from; i--) {
+      this.arr[i] = this.arr[i - 1];
+    }
+  }
+}
---
+++ src/day14/Stack.ts
@@ -0,0 +1,40 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+}
+
+export default class Stack<T> {
+    public length: number;
+    private head?: Node<T>;
+
+
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+    }
+
+    push(item: T): void {
+        this.length++;
+        const node = {value: item} as Node<T>;
+
+        node.next = this.head;
+        this.head = node;
+    }
+
+    pop(): T | undefined {
+        if (!this.head) {
+            return;
+        }
+        this.length--;
+        const poppingNode = this.head;
+
+        this.head = poppingNode.next;
+        poppingNode.next = undefined;
+
+        return poppingNode.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
