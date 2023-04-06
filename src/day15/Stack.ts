3526bc9dd4eebaf295daf4f1a25ec7f402c1fc97
nacknime-official <nacknime.official@gmail.com>
2023-04-06 22:06:02 +0300

    add day 15

---
+++ src/day15/Stack.ts
@@ -0,0 +1,41 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+}
+
+export default class Stack<T> {
+    public length: number;
+    private head?: Node<T>;
+
+
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+    }
+
+    push(item: T): void {
+        const node = {value: item} as Node<T>;
+
+        node.next = this.head;
+        this.head = node;
+
+        this.length++;
+    }
+
+    pop(): T | undefined {
+        if (!this.head) {
+            return;
+        }
+
+        const poppingNode = this.head;
+        this.head = this.head.next;
+        poppingNode.next = undefined;
+
+        this.length--;
+        return poppingNode.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
